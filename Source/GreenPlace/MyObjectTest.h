6f182ed673620068483316250d3b1df14d5fc53f
John <ufa-john@yandex.ru>
2020-12-01 17:29:22 +0500

    123

---
+++ Source/GreenPlace/MyObjectTest.h
@@ -0,0 +1,15 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "MyObjectTest.generated.h"
+
+UCLASS()
+
+class GREENPLACE_API UMyObjectTest : public UObject
+{
+	GENERATED_BODY()
+	
+};
